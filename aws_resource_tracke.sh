8fc3046424e4379b4201862e45824ac8a52a9872
Deep <deep.mukherjee82@gmail.com>
2025-04-17 06:50:32 +0000

    This is my 1st verison

---
+++ aws_resource_tracke.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#set -x
+
+#Lists S3 buckets
+echo "Lists of S3 buckets"
+aws s3 ls
+
+#Lists EC2 instances
+echo "Lists of EC2 instances"
+#aws ec2 describe-instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#Lists Lambda 
+echo "Lists of Lambda functions"
+aws lambda list-functions
+
+#Lists IAM users
+echo "Lists of IAM users"
+aws iam list-users
